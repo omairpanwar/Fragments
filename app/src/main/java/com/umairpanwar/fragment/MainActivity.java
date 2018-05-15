407f044d82cb87dbede85035d7389d23cc209a25
Omair Panwar <umairpanwar123@gmail.com>
2018-05-15 14:57:54 +0500

    first commit

---
+++ app/src/main/java/com/umairpanwar/fragment/MainActivity.java
@@ -0,0 +1,19 @@
+package com.umairpanwar.fragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container,new HomeFragment())
+                .commit();
+
+    }
+}
